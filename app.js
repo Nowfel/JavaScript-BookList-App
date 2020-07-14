5a567eebbce62986bb5e45c7a8f2b5661ec0a124
nowfel <abdullahalmahfuj.cse@gmail.com>
2020-07-14 07:06:40 +0600

    clearFields added

--- app.js
+++ app.js
@@ -29,19 +29,34 @@ class UI {
 
         list.appendChild(row);
     }
+
     static deleteBook(el) {
         if (el.classList.contains('delete')) {
             el.parentElement.parentElement.remove();
         }
     }
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        // Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
         document.querySelector('#isbn').value = '';
     }
-
-
 }
+
+
+
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
